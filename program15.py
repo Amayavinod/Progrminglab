e6649813d55421d7ff0bfec78253e0cb9ec8c92e
Amayavinod <74695779+Amayavinod@users.noreply.github.com>
2021-02-06 10:10:06 +0530

    Add files via upload

---
+++ program15.py
@@ -0,0 +1,13 @@
+def factoriyal(n):
+	if n < 0:
+		return 0
+	elif n == 0 or n == 1:
+		return 1
+	else:
+		fact=1
+		while(n>1):
+			fact *=n
+			n-=1
+		return fact
+num=int(input("enter the number"))
+print("factoriyal of",num,"is",factoriyal(num))
